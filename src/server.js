120bd6e794078ed2b6f5261b99e5fb1ee07960b5
BianQt <bayanqutshan@gmail.com>
2021-11-08 22:13:01 +0200

    test for deploy

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@
 
 // 3rd Party Resources
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+// const cors = require('cors');
+// const morgan = require('morgan');
 
 // Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
@@ -14,8 +14,8 @@ const authRoutes = require('./auth/routes.js');
 const app = express();
 
 // App Level MW
-app.use(cors());
-app.use(morgan('dev'));
+// app.use(cors());
+// app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
